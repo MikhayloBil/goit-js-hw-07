d9b9d900a863d90e659845dc4f9b9a5a8c8f3d9a
MikhayloBil <152810994+MikhayloBil@users.noreply.github.com>
2024-04-11 11:36:47 +0300

    start

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.body;
+const span = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
