cda5ff742ecdd9809f8d8ee60ddab9a8e782e11a
MikhayloBil <152810994+MikhayloBil@users.noreply.github.com>
2024-04-11 11:30:20 +0300

    start

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = {};
+  const formInputs = event.target.elements;
+
+  for (let input of formInputs) {
+    if (input.nodeName === "INPUT") {
+      const trimmedValue = input.value.trim();
+      if (!trimmedValue) {
+        alert("All form fields must be filled in");
+        return;
+      }
+      formData[input.name] = trimmedValue;
+    }
+  }
+
+  console.log(formData);
+  event.target.reset();
+});
